e314f3bb7b2d14e6bf83c30662d799fbe454d470
Alex Moskvin <moskwin.a.va@gmail.com>
2022-07-16 20:52:50 +0300

    добавлена задача запуска, переписал main()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
+class err_c
+{
+    static const std::size_t msgcnt = 32;
+    static const char *messages[msgcnt];
+
+    int _err;
+
+public:
+    enum {
+        OK, NULL_POINTER, OUT_OF_RANGE, INVALID_ARG, UNKNOWN_COMMAND,
+    };
+
+    err_c(int err) : _err(err) { }
+    const char *operator()() { return messages[_err]; }
+};
+
+const char *err_c::messages[err_c::msgcnt] = {
+         "OK", "null pointer", "out of range", "invalid arg", "unknown command"
+};
+
+int foo(void *p) {
+    if (!p) {
+        // can not work. Return
+        return err_c::NULL_POINTER;
+    } else {
+        // do any work here
+        // ...
+        return 0;
+    }
+}
+
 int main (void)
 {
-    std::cout << "hello, fucking world!" << std::endl;
+    static int x;
+    std::cout << "foo result is: " << err_c(foo(&x))() << std::endl;
+    std::cout << "foo result is: " << err_c(foo(nullptr))() << std::endl;
     return 0;
 }
