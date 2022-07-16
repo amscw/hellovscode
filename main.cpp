05f8b22f5f4b3ee334834c2474c10fd5bb4b257f
Alex Moskvin <moskwin.a.va@gmail.com>
2022-07-16 15:35:37 +0300

    минимальный набор для простейшего приложения

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main (void)
+{
+    std::cout << "hello, fucking world!" << std::endl;
+    return 0;
+}
---
+++ mingw64.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+
+set(CMAKE_C_COMPILER "gcc.exe")
+set(CMAKE_ASM_COMPILER "as.exe")
+set(CMAKE_CXX_COMPILER "g++.exe")
+
+set(CMAKE_OBJCOPY "objcopy.exe" CACHE INTERNAL "objcopy tool")
+set(CMAKE_SIZE_UTIL "size.exe" CACHE INTERNAL "size tool")
