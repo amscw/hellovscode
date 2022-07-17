f6a39bda13635e4653b8578496dc97c8570920f4
Alex Moskvin <moskwin.a.va@gmail.com>
2022-07-17 19:13:17 +0300

    добавлено подключение библиотеки boost. Немного кода для main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "boost/type_index.hpp"
 
 class err_c
 {
@@ -33,8 +34,20 @@ int foo(void *p) {
 
 int main (void)
 {
+    using boost::typeindex::type_id_with_cvr;
+
     static int x;
     std::cout << "foo result is: " << err_c(foo(&x))() << std::endl;
     std::cout << "foo result is: " << err_c(foo(nullptr))() << std::endl;
+
+    auto x1 = {0};
+    auto x2 = 0;
+    std::cout << "typeof(x1): " << type_id_with_cvr<decltype(x1)>().pretty_name() << std::endl;
+    std::cout << "typeof(x2): " << type_id_with_cvr<decltype(x2)>().pretty_name() << std::endl;
+
+    auto arg = x2;
+    std::cout << "typeof(arg): " << type_id_with_cvr<decltype(arg)>().pretty_name() << std::endl;
+    std::cout << "typeof((arg)): " << type_id_with_cvr<decltype((arg))>().pretty_name() << std::endl;
+    
     return 0;
 }
